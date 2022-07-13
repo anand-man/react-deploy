c78424d00ef88e7364a812f82998b9b31c2a8026
Anand <35867291+anand-man@users.noreply.github.com>
2022-07-13 15:55:20 +0530

    Update action.js

--- capstone-project-2-source-code/src/store/action.js
+++ capstone-project-2-source-code/src/store/action.js
@@ -19,7 +19,7 @@ export const loadProducts = () => {
             dispatch(getProducts(res.data));
         }).catch((error) => {
             error = new Error("Something went wrong!");
-            dispatch(getProduct(error));
+            dispatch(getProducts(error));
         });
     };
 };
